2fea5a1be1e13d99626cc4b144ee4a26e375f1c0
arya <arya.abawi@gmail.com>
2023-01-14 16:47:45 -0500

    added keystroke input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use itertools::Itertools;
 use rand::prelude::*;
+use std::convert::TryFrom;
 
 const TILE_SIZE: f32 = 40.0;
 const TILE_SPACER: f32 = 10.0;
@@ -67,6 +68,7 @@ fn main() {
             spawn_tiles,
         )
         .add_system(render_tile_points)
+        .add_system(board_shift)
         .run()
 }
 
@@ -189,5 +191,51 @@ fn render_tile_points(
             let mut text_section = text.sections.first_mut().expect("expect first section to be accessible as mutable");
             text_section.value = points.value.to_string()
         }
+    }
+}
+
+enum BoardShift {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+impl TryFrom<&KeyCode> for BoardShift {
+    type Error = &'static str;
+
+    fn try_from(
+        value: &KeyCode,
+    ) -> Result<Self, Self::Error> {
+        match value {
+            KeyCode::Left => Ok(BoardShift::Left),
+            KeyCode::Up => Ok(BoardShift::Up),
+            KeyCode::Right => Ok(BoardShift::Right),
+            KeyCode::Down => Ok(BoardShift::Down),
+            _ => Err("not a valid board_shift key"),
+        }
+    }
+}
+
+fn board_shift(keyboard_input: Res<Input<KeyCode>>) {
+    let shift_direction = 
+        keyboard_input.get_just_pressed().find_map(
+            |key_code| BoardShift::try_from(key_code).ok(),
+        );
+    
+    match shift_direction {
+        Some(BoardShift::Left) => {
+            dbg!("left");
+        }
+        Some(BoardShift::Right) => {
+            dbg!("right");
+        }
+        Some(BoardShift::Up) => {
+            dbg!("up");
+        }
+        Some(BoardShift::Down) => {
+            dbg!("down");
+        }
+        None => (),
     }
 }
