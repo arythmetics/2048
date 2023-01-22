8a5f63f75d89413a73289e369e1f1f3a9aea6e91
arya <arya.abawi@gmail.com>
2023-01-22 12:37:22 -0500

    tiles physically shift on the board

--- src/main.rs
+++ src/main.rs
@@ -69,6 +69,7 @@ fn main() {
         )
         .add_system(render_tile_points)
         .add_system(board_shift)
+        .add_system(render_tiles)
         .run()
 }
 
@@ -285,5 +286,24 @@ fn board_shift(
             dbg!("down");
         }
         None => (),
+    }
+}
+
+fn render_tiles(
+    mut tiles: Query<(
+        &mut Transform,
+        &Position,
+        Changed<Position>,
+    )>,
+    query_board: Query<&Board>,
+) {
+    let board = query_board
+        .single();
+    for (mut transform, pos, pos_changed) in tiles.iter_mut()
+    {
+        if pos_changed {
+            transform.translation.x = board.cell_position_to_physical(pos.x);
+            transform.translation.y = board.cell_position_to_physical(pos.y);
+        }
     }
 }
